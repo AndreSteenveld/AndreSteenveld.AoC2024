369149aea3f4d2c1f8e4709fbd000a2917c70798
AndreSteenveld <andre@robotx.eu>
2024-12-07 22:53:23 +0100

    Day 6 - part 2

--- AndreSteenveld.AoC2024.Day06/Program.cs
+++ AndreSteenveld.AoC2024.Day06/Program.cs
@@ -1,41 +1,93 @@
 ﻿
 using AndreSteenveld.AoC;
+using static AndreSteenveld.AoC.Functions;
 
 using Coordinate = (int x, int y, Direction d);
 
-var (stack, area, max_x, max_y) = Console.In.ReadLines().Aggregate(
-    (new Stack<Coordinate>(), String.Empty, 0, 0),
+WaitForDebugger();
+
+var (start, area, max_x, max_y) = Console.In.ReadLines().Aggregate(
+    ((Coordinate?)null, String.Empty, 0, 0),
     (result, line) => {
 
-        var (stack, map, max_x, max_y) = result;
+        var (start, map, max_x, max_y) = result;
 
         max_x = line.Length switch {
             var v when max_x is 0 => v,
             var v when max_x == v => v,
             _ => throw new Exception()
         };
-        
-        var start = (x : line.IndexOf((char)Direction.Up), max_y, Direction.Up);
 
-        if (start.x is not -1) {
+        if (start is not null) 
+            return (start, String.Concat(map, line), max_x, max_y + 1);
+        
+        var x = line.IndexOf((char)Direction.Up);
+
+        if (x is not -1) {
+            start = (x : line.IndexOf((char)Direction.Up), max_y, Direction.Up);
             line = line.Replace((char)Direction.Up, (char)Field.Empty);
-            stack.Push(start);
         }
-         
-        return (stack, String.Concat(map, line), max_x, max_y+1);
+
+        return (start, String.Concat(map, line), max_x, max_y + 1);
     }
 );
 
+
+if (start is null)
+    throw new Exception("No starting position");
+
+var ByCoordinates = EqualityComparer<Coordinate>.Create(
+    (left, right) => (left.x, left.y) == (right.x, right.y),
+    coordinate => coordinate.x ^ coordinate.y
+);
+
 var map = new Map(max_x, max_y, area);
+var route = Patrol(map, (Coordinate)start!, (m, c) => m.WithinBounds(c.x, c.y)).ToList();
 
-for (var (x, y, d) = stack.Peek(); map.WithinBounds(x, y); (x, y, d) = stack.Peek())
-    stack.Push(Step(map, (x, y, d)));
-
-var distinct_locations_visited = stack.ToList().DistinctBy(c => (c.x, c.y)).Count() - 1;
+var distinct_locations_visited = route[..^1].Distinct(ByCoordinates).Count();
 
 Console.WriteLine($"Distinct locations visited :: {distinct_locations_visited}");
 
+var number_of_obstacles_placeable_for_infinte_loop = route[..^1].AsParallel()
+    .Where((coordinate, index) => {
+        var step = Step(map, coordinate);
+
+        return map.WithinBounds(step.x, step.y)
+               && ByCoordinates.Equals(coordinate, step) is false // Filter out rotations
+               && route[0..index].Contains(step, ByCoordinates) is false; // Make sure we haven't already walked there to come to this point
+    })
+    .Count(coordinate => {
+        var index = route.IndexOf(coordinate); 
+        var (x, y, _ ) = Step(map, coordinate);
+
+        var map_with_hypothetical_obstruction = map.PlaceObstacle(x, y);
+        var hypothetical_path = new List<Coordinate>();
+
+        foreach (var step in Patrol(map_with_hypothetical_obstruction, coordinate, (m, s) => m.WithinBounds(s.x, s.y) && route[0..index].Contains(s) is false)) {
+            if (hypothetical_path.Contains(step))
+                break;
+            hypothetical_path.Add(step);
+        }
+
+        var (last_x, last_y, _) = hypothetical_path[^1];
+
+        return map_with_hypothetical_obstruction.WithinBounds(last_x, last_y);
+    });
+
+Console.WriteLine($"Distinct locations that can be obstructed :: {number_of_obstacles_placeable_for_infinte_loop}");
+
 return;
+
+IEnumerable<Coordinate> Patrol(Map map, Coordinate start, Func<Map, Coordinate, bool> can_make_next_step) {
+    var (x, y, d) = start;
+
+    yield return (x, y, d);
+
+    while (can_make_next_step(map, (x, y, d))){
+        (x, y, d) = Step(map, (x, y, d));
+        yield return (x, y, d);
+    }
+}
 
 Coordinate Step(Map map, Coordinate current) {
     var (x, y, d) = current;
@@ -71,8 +123,8 @@ public record Map(int MaxX, int MaxY, string Area) {
 
     int to_index(int x, int y) => (x + (y * MaxX));
 
-    public Map PlaceObstacle((int x, int y) c) => 
-        this with { Area = Area.ReplaceAt(to_index(c.x, c.y), (char)Field.Obstacle) };
+    public Map PlaceObstacle(int x, int y) => 
+        this with { Area = Area.ReplaceAt(to_index(x, y), (char)Field.Obstacle) };
     
     public char? this[int x, int y] => WithinBounds(x, y) ? Area[to_index(x, y)] : null;
 }
