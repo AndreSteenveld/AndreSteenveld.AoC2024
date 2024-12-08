55c4807980fc63ca703b68ae16755f9d2b416edd
AndreSteenveld <andre@robotx.eu>
2024-12-08 22:57:53 +0100

    Day 8

---
+++ AndreSteenveld.AoC/Map.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace AndreSteenveld.AoC;
+
+public static class Coordinate {
+    public static bool WithinBoundsOf(this (int x, int y) coordinate, Map map) =>
+        map.WithinBounds(coordinate.x, coordinate.y);
+} 
+
+public record Map(int Width, int Height, string Area) : IEnumerable<(int x, int y, char f)> {
+
+    public static Map FromInputLines(Map? map, string line) {
+
+        map ??= new Map(0, 0, String.Empty);
+        
+        return new Map(
+            Width: map.Width == 0           ? line.Length 
+                 : map.Width == line.Length ? line.Length 
+                 : throw new Exception("Jagged map"), 
+            Height: map.Height + 1, 
+            Area: map.Area + line
+        );
+
+    }
+
+    public bool WithinBounds((int x, int y) c) => WithinBounds(c.x, c.y);
+    public bool WithinBounds(int x, int y) => x >= 0 && x < Width &&
+                                              y >= 0 && y < Height;
+
+    int to_index(int x, int y) => (x + (y * Width));
+    (int x, int y) to_coordinate(int i) => (i % Width, i / Height);
+    
+    public char? this[int i] => i < 0 ? null : i > Area.Length ? null : Area[i];
+    public char? this[int x, int y] => WithinBounds(x, y) ? Area[to_index(x, y)] : null;
+
+    public IEnumerable<(int x, int y, char f)> Fields => this;
+    
+    IEnumerator<(int x, int y, char f)> IEnumerable<(int x, int y, char f)>.GetEnumerator() {
+        for (int i = 0; i < Area.Length; i++) {
+            var (x, y) = to_coordinate(i);
+            yield return (x, y, Area[i]);
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+        return ((IEnumerable<(int, int, int)>)this).GetEnumerator();
+    }
+}
---
+++ AndreSteenveld.AoC2024.Day08/Program.cs
@@ -0,0 +1,59 @@
+﻿using AndreSteenveld.AoC;
+
+var map = Console.In.ReadLines().Aggregate((Map)null!, Map.FromInputLines);
+
+var stations = map.Fields
+    .Where( field => Char.IsLetterOrDigit(field.f) )
+    .Aggregate( new Dictionary<char, List<(int x, int y)>>(), (dictionary, station) => {
+        var (x, y, f) = station;
+        
+        if(dictionary.TryGetValue(f, out var stations))
+            stations.Add((x, y));
+        else 
+            dictionary[f] = [(x, y)];
+        
+        return dictionary;
+    });
+    
+var antinodes = stations.ToDictionary(
+        kv => kv.Key,
+        kv => Enumerable
+            .Distinct(
+                from station in kv.Value
+                from other in kv.Value
+                let offset = ( x : station.x - other.x, y : station.y - other.y) where offset is not (0,0)
+                let antinode = (station.x + offset.x, station.y + offset.y) where antinode.WithinBoundsOf(map)
+                select antinode
+            )
+            .ToArray()
+    );
+
+var number_of_antinodes = antinodes.SelectMany(kv => kv.Value).Distinct().Count();
+
+Console.WriteLine($"Total number of antinodes :: {number_of_antinodes}");
+
+var projected_antinode_lines = stations.ToDictionary(
+    kv => kv.Key,
+    kv => 
+        (
+            from station in kv.Value
+            from other in kv.Value
+            let offset = (x: station.x - other.x, y: station.y - other.y) where offset is not (0, 0)
+            select (station, offset)
+        )
+        .SelectMany( so => {
+            var (station, offset) = so;
+
+            return Enumerable
+                .Range(1, Int32.MaxValue)
+                .Select(step => (station.x + offset.x * step, station.y + offset.y * step))
+                .TakeWhile(map.WithinBounds)
+                .Append(station);
+        })
+        .Distinct()
+        .ToArray()
+    );
+
+var number_of_antinodes_on_line = projected_antinode_lines.SelectMany(kv => kv.Value).Distinct().Count();
+
+Console.WriteLine($"Total number of projected antinode lines :: {number_of_antinodes_on_line}");
