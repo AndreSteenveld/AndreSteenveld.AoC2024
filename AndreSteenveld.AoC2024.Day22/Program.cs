eee1f4b909529bea2b15f1b49537f741c0d6e186
AndreSteenveld <andre@robotx.eu>
2024-12-22 13:23:39 +0100

    Day 22 P1, do it in a single expression is key

---
+++ AndreSteenveld.AoC2024.Day22/Program.cs
@@ -0,0 +1,23 @@
+﻿using AndreSteenveld.AoC;
+
+var seeds = Console.In.ReadLines().Select(Int64.Parse).ToArray();
+
+var sum = seeds.Select(seed => RandomizeUsingSeed(seed).Skip(1999).First()).Sum();
+
+Console.WriteLine($"Part 1 sum :: {sum}");
+
+return;
+
+IEnumerable<long> RandomizeUsingSeed(long secret) {
+    
+    while (true) {
+        secret = (secret ^ (secret * 64)) % 16777216;
+        secret = (secret ^ (secret / 32)) % 16777216;
+        secret = (secret ^ (secret * 2048)) % 16777216;
+
+        yield return secret;
+    }
+
+} 
+
+file static class _ { }
